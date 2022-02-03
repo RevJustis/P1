819540dc4f9f41db701304d74daf92172fb95c85
RevJustis <97845336+RevJustis@users.noreply.github.com>
2022-02-03 11:10:28 -0800

    Modifies HiveTest2.scala Comments out sql lines and adds some comments with different config options for testing and troubleshooting.

--- src/test/scala/HiveTest2.scala
+++ src/test/scala/HiveTest2.scala
@@ -3,9 +3,15 @@ import org.apache.spark.sql.SparkSession
 object HiveTest2 {
 
   def main(args: Array[String]): Unit = {
+    // OTHER CONFIG OPTIONS
+    //("spark.master", "hdfs://localhost:9000/"
+    //.master("yarn"/ or maybe "hive")
+    //config("spark.master", "local")
+    //.config("javax.jdo.option.ConnectionURL", "jdbc:derby:/Users/justis/hadoop/metastore_db")
+    //.config("hive.metastore.warehouse.dir", "hdfs://localhost:9000/user/hive/warehouse")
     val spark = SparkSession.builder()
       .appName("HiveTest2")
-      .config("spark.master", "local")
+      .master("local")
       .enableHiveSupport()
       .getOrCreate()
     println("created spark session")
@@ -13,8 +19,10 @@ object HiveTest2 {
     //spark.sql("CREATE TABLE IF NOT EXISTS src(key INT, value STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY ‘,’ STORED AS TEXTFILE")
     //spark.sql("LOAD DATA LOCAL INPATH 'input/kv1.txt' INTO TABLE src")
     //spark.sql("CREATE TABLE IF NOT EXISTS src (key INT,value STRING) USING hive")
-    spark.sql("create table newone1(id Int,name String) row format delimited fields terminated by ','")
-    spark.sql("LOAD DATA LOCAL INPATH 'input/kv1.txt' INTO TABLE newone1")
-    spark.sql("SELECT * FROM newone1").show()
+
+    //spark.sql("create table newone1(id Int,name String) row format delimited fields terminated by ','")
+    //spark.sql("LOAD DATA LOCAL INPATH 'input/kv1.txt' INTO TABLE newone1")
+    //spark.sql("SELECT * FROM newone1").show()
+
   }
 }
