00cbe81ebf62220a521607606f53949cd837680d
RevJustis <97845336+RevJustis@users.noreply.github.com>
2022-02-08 16:37:01 -0800

    Base functionality and labeling complete! Next step, visualization! Zeppelin maybe...

--- src/main/scala/P1.scala
+++ src/main/scala/P1.scala
@@ -2,8 +2,8 @@ import org.apache.spark.sql.SparkSession
 
 import scala.io.StdIn.{readByte, readChar, readLine}
 import Utilities._
-
-import java.io.{PrintWriter, File}
+import org.apache.spark.sql.functions.col
+import java.io.{File, PrintWriter}
 import scala.Console.println
 
 object P1 {
@@ -61,7 +61,7 @@ object P1 {
 
   def main(args: Array[String]): Unit = {
     //<editor-fold desc="Set configuration">
-//    spark.sql("set hive.exec.dynamic.partition.mode=nonstrict")// TODO USE THIS FOR A MORE COMPACT DELETE
+    spark.sql("set hive.exec.dynamic.partition.mode=nonstrict")// TODO USE THIS FOR A MORE COMPACT DELETE
     //</editor-fold>
     //<editor-fold desc="DROP commands">
     //spark.sql("DROP TABLE IF EXISTS branchbevs")
@@ -97,7 +97,7 @@ object P1 {
 //      "ROW FORMAT DELIMITED FIELDS TERMINATED BY ','")
 //    spark.sql("CREATE TABLE IF NOT EXISTS cons_c (bev STRING, count INT)" +
 //      "ROW FORMAT DELIMITED FIELDS TERMINATED BY ','")
-//    spark.sql("CREATE TABLE IF NOT EXISTS Partitioned_abc(bev STRING) COMMENT 'A PARTITIONED BRANCH TABLE' PARTITIONED BY (branches STRING)")
+//    spark.sql("CREATE TABLE IF NOT EXISTS Partitioned_abc(bev STRING) PARTITIONED BY (branches STRING)")
 
     //</editor-fold>
     //<editor-fold desc="Loads into tables">
@@ -109,6 +109,7 @@ object P1 {
     //    spark.sql("LOAD DATA LOCAL INPATH 'input/Bev_ConscountB.txt' INTO TABLE cons_b")
     //    spark.sql("LOAD DATA LOCAL INPATH 'input/Bev_ConscountC.txt' INTO TABLE cons_c")
 //    spark.sql("INSERT OVERWRITE TABLE Partitioned_abc PARTITION(branches) SELECT bev,branch from all_branch")
+
     //</editor-fold
     //<editor-fold desc="Create table from query and MISC">
 //    Intersection of cons_a and cons_b \\
@@ -129,21 +130,21 @@ object P1 {
 //      pw.write((c(0)(0)).toString + ',' + c(0)(1) + '\n')
 //      pw.close()
 //    }
-    for (x <- 1 to 9) {
-      spark.sql(s"CREATE TABLE IF NOT EXISTS consTot$x (branch INT, consTot INT) ROW FORMAT DELIMITED FIELDS TERMINATED BY ','")
-      spark.sql(s"LOAD DATA LOCAL INPATH 'input/dumb$x.txt' OVERWRITE INTO TABLE consTot$x")
-    }
+//    for (x <- 1 to 9) {
+//      spark.sql(s"CREATE TABLE IF NOT EXISTS consTot$x (branch INT, consTot INT) ROW FORMAT DELIMITED FIELDS TERMINATED BY ','")
+//      spark.sql(s"LOAD DATA LOCAL INPATH 'input/dumb$x.txt' OVERWRITE INTO TABLE consTot$x")
+//    }
 //    var s = "CREATE TABLE IF NOT EXISTS bevTotAll AS SELECT * FROM bevTot1 "
 //    for (x <- 2 to 9) {
 //      s = s + s"UNION SELECT * FROM bevTot$x "
 //    }
 //    spark.sql(s)
 
-    var s2 = "CREATE TABLE IF NOT EXISTS constotall AS SELECT * FROM constot1 "
-    for (x <- 2 to 9) {
-      s2 = s2 + s"UNION SELECT * FROM constot$x "
-    }
-    spark.sql(s2)
+//    var s2 = "CREATE TABLE IF NOT EXISTS constotall AS SELECT * FROM constot1 "
+//    for (x <- 2 to 9) {
+//      s2 = s2 + s"UNION SELECT * FROM constot$x "
+//    }
+//    spark.sql(s2)
     //    bevs common between BranchA and ConscountA \\
 //      spark.sql("SELECT branch_a.branch, cons_a.bev, cons_a.count FROM branch_a " +
 //        "INNER JOIN cons_a ON cons_a.bev = branch_a.bev ORDER BY branch_a.branch, cons_a.bev, cons_a.count").show()
@@ -162,10 +163,9 @@ object P1 {
       option match {
         case "Scenario 1" =>
           println("Total for Branch 1")
-          //spark.sql("SELECT * FROM totCon1").show()
+          spark.sql("SELECT * FROM totCon1").show()
           println("Total for Branch 2")
-          val v = 2
-          spark.sql(s"SELECT $v AS branch, SUM(count) AS ConsBranch2 FROM b2bevs INNER JOIN cons_abc AS c ON c.bev = b2bevs.bev").show()
+          spark.sql(s"SELECT 2 AS branch, SUM(count) AS ConsBranch2 FROM b2bevs INNER JOIN cons_abc AS c ON c.bev = b2bevs.bev").show()
         case "Scenario 2" =>
           println("Most consumed beverage on branch 1")
           spark.sql("SELECT b1bevs.bev, count FROM b1bevs INNER JOIN cons_abc AS c ON c.bev = b1bevs.bev " +
@@ -181,6 +181,14 @@ object P1 {
         case "Scenario 4" => // Create a "partiion,View" for scenario 3
           println("Create a partition for Scenario 3")
           spark.sql("SELECT * FROM Partitioned_abc WHERE branches = 'Branch9'").show
+          spark.sql("describe formatted Partitioned_abc").show()
+          /* //some alternate partition code testing
+          val df = spark.sql("SELECT bev, branch FROM all_branch")
+          println("Below is a partition ??????????????")
+          println(df.repartition(9, col("branch")).where("branch == 'Branch5'").count())
+          println("Create a partition for Scenario 3 REDUX")
+          println(spark.sql("SELECT * FROM Partitioned_abc").where("branches == 'Branch5'").count)
+           */
         case "Scenario 5" => // Add note to a table
           val note = readLine("In this scenario you get to add a note to the branch_a table!\nPlease enter your note here: ")
           spark.sql(s"ALTER TABLE branch_a SET TBLPROPERTIES('notes' = '$note')")
@@ -199,8 +207,18 @@ object P1 {
           spark.sql("INSERT OVERWRITE TABLE branch_a select * from branch_a_del")
           spark.sql("DROP TABLE branch_a_del")
           spark.sql("SELECT * FROM branch_a").show(9999)
-        case "Scenario 6" =>// TODO currently just a place to dump test queries. Should be My Query: Variety and Diversity
-          spark.sql("SELECT constotall.branch, (bevTot / consTot) AS Diversity_Rank  FROM constotall " +
+        case "Scenario 6" =>
+          println("My future query:\n")
+          println("The Diversity Rank gives an indication of how diverse a branches orders are. For reverence, perfect\n" +
+            "diversity (every customer has a unique order) among 10 total drinks types sold will cause Diversity Rank\n" +
+            "= 10, perfect diversity among 25 total drink types sold will cause Diversity Rank 25. At half diversity\n" +
+            "(10 different drinks ordered by 20 different people for example, then the rank will be 5. As the number\n" +
+            "of customers grows while number of drinks ordered stays the same or shrinks, the rank will get closer and\n" +
+            "closer to 0 (but never reach 0). But, if every single item on the menu was ordered, diversity no longer\n" +
+            "has significant weight and to indicate this the rank will be made zero. This means you both know how many\n" +
+            "items on the menu were ordered, how diverse your customers orders are, and whether or not this metric\n" +
+            "contextually valuable. When was the last time business data told you when it is and isn't useful?")
+          spark.sql("SELECT constotall.branch, ((bevTot / consTot) * (bevTot % 54)) AS Diversity_Rank  FROM constotall " +
             "INNER JOIN bevTotAll ON constotall.branch = bevTotAll.branch").show
         case "End Program" => continue = false
       }
